14aeba38d27190ad02a69664349f9fd6d980df95
Andrea Williams <anw0514@gmail.com>
2019-01-02 15:48:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves = array.new(3))
+  dwarves.each_with_index { |dwarf, x| puts "#{x + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls = array.new(5))
+  planteer_calls.map! { |element| element.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
