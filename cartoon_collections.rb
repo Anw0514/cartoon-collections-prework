a5b4b094385576b7e31e871299d447a3cbbdbe79
Andrea Williams <anw0514@gmail.com>
2019-01-02 15:53:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each { |element| if cheese_types.include?(element) == true {return element}}
+  array.each { |element| if cheese_types.include?(element) == true then return element end}
 end
